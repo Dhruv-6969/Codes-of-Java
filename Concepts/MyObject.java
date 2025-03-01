be01b5d56c4d799561dcbfde951a26c7af215858
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-03-01 12:28:06 +0530

    modified:   Concepts/MyObject.class 	modified:   Concepts/MyObject.java 	modified:   Concepts/Second.class

--- Concepts/MyObject.java
+++ Concepts/MyObject.java
@@ -1,22 +1,41 @@
-// public class Object{
+// public class MyObject{
 
 //     int x = 5;
 //     public static void main(String[] args) {
-//         Object myObj1 = new Object();
-//         Object myObj2 = new Object();
+//         MyObject myObj1 = new MyObject();
+//         MyObject myObj2 = new MyObject();
 //         System.out.println(myObj1.x);
 //         System.out.println(myObj2.x);
 //     }
 // }
 
 public class MyObject{
-    int x = 5;
+    void myMethod1(){
+        System.out.println("I am Method 1");
+    }
+
+    void myMethod2(){
+        System.out.println("I am Method 2");
+    }
 }
 
 class Second{
 
     public static void main(String[] args) {
         MyObject myObj = new MyObject();
-        System.out.println(myObj.x);
+        myObj.myMethod1();
+        myObj.myMethod2();
     }
 }
+
+// public class MyObject{
+
+//     public void myMethod(){
+//         System.out.println("Hello World");
+//     }
+
+//     public static void main(String[] args) {
+//         MyObject obj = new MyObject();
+//         obj.myMethod();
+//     }
+// }
