85c5217489f76819a2bb5abacc5b0cafdcbc0554
Dhruv-6969 <152963213+Dhruv-6969@users.noreply.github.com>
2025-03-03 19:17:01 +0530

    modified:   Concepts/Abc.java

--- Concepts/Abc.java
+++ Concepts/Abc.java
@@ -1,21 +1,6 @@
-// class NestedClass {
-
-//     protected static class hey{
-//         private String name;
-//         private int age;
-//     }
-// }
-
-// public class Abc{
-//     public static void main(String[] args) {
-//         NestedClass.hey hehe = new NestedClass.hey();
-
-//     }
-// }
-
 class NestedClass {
 
-    protected class hey{
+    protected static class hey{
         private String name;
         private int age;
     }
@@ -23,7 +8,22 @@ class NestedClass {
 
 public class Abc{
     public static void main(String[] args) {
-        NestedClass hihi = new NestedClass();
-        NestedClass.hey hehe = hihi.new hey();
+        NestedClass.hey hehe = new NestedClass.hey();
+
     }
 }
+
+// class NestedClass {
+
+//     protected class hey{
+//         private String name;
+//         private int age;
+//     }
+// }
+
+// public class Abc{
+//     public static void main(String[] args) {
+//         NestedClass hihi = new NestedClass();
+//         NestedClass.hey hehe = hihi.new hey();
+//     }
+// }
